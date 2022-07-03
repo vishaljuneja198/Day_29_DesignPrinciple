22bd8255526ad0be0ef615202221f458f23cbc80
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-07-03 23:31:01 +0530

    added UC1 load file from CSV

--- src/main/java/com/bridge/labz/CensusAnalyserException.java
+++ src/main/java/com/bridge/labz/CensusAnalyserException.java
@@ -13,10 +13,14 @@ public class CensusAnalyserException extends Exception {
     public CensusAnalyserException(String message, ExceptionType type) {
         super(message);
         this.type = type;
+
+
     }
 
     public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
         super(message, cause);
+
+        
         this.type = type;
     }
 }
